a086904320d04934c5beab8c87712bba336c97f8
renanrecio <renanrecio@gmail.com>
2019-01-12 13:19:47 -0200

    config

--- www/config_app.js
+++ www/config_app.js
@@ -1,5 +1,5 @@
-var id_app = '1';
+var id_app = '2';
 
-var versao_app = 'v1.0_11_01_2019';//'v1.0_05_02_2018';
+var versao_app = 'v1.0_12_01_2019';//'v1.0_05_02_2018';
 
-var nome_do_app = "PetShop";
+var nome_do_app = "app_casamento_template";
